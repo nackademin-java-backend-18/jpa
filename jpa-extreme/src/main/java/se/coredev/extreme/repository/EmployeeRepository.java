579f7c8e2e586a844fc7f3feff572ee6dee9e743
Anders Carlsson <anders.carlsson@coredev.se>
2018-03-28 14:47:35 +0200

    Cleanup

--- jpa-extreme/src/main/java/se/coredev/extreme/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package se.coredev.extreme.repository;
-
-import se.coredev.extreme.data.Employee;
-
-public interface EmployeeRepository extends CrudRepository<Employee> {
-    Employee getByEmployeeNumber(String number);
-}
